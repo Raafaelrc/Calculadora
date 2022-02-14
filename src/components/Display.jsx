bfeadeffbd253b8faef9ce97314a46f459c5b8af
Rafael <rafa15963@gmail.com>
2022-02-14 12:24:17 -0300

    alteração

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import './Display.css'
 
-export default  (props) =>  {
-    <div className="display"> {props.value } </div>
 
+const disp = (props) => {
+    return(
+        <div className="display"> {props.value } </div>
+
+    )
 }
+export default  disp
+    
+
+
